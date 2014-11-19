d48eae7e36b105b088412bd181c6a7b7bcd34eca
mk <manojklm@gmail.com>
2014-11-20 01:27:41 +0530

    imports + http status codes

--- filepuller/filepuller_server.py
+++ filepuller/filepuller_server.py
@@ -1,1 +1,27 @@
+"""
+filepuller server
+=================
+"""
+import datetime
+import os
+import sys
+import zlib
+import hashlib
+from threading import Thread
+import http.server, select, socket, socketserver, urllib.parse
+import pickle
+import binascii
+from path import Path
+
 __author__ = 'M'
+
+port = 80
+drv = 'd:'
+bind_ip = '0.0.0.0'
+DIR_SRC_BASE = 'src'
+
+
+HEADER_200 = b'''HTTP/1.1 200 OK\n\n'''
+HEADER_404 = b'''HTTP/1.1 404 File Not Found\n\n'''
+
+
