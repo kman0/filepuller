3a900c31709b5b225fee7418f89da1f063bf2018
mk <manojklm@gmail.com>
2014-11-21 02:29:12 +0530

    bare bone server_instance

--- filepuller/filepuller_server.py
+++ filepuller/filepuller_server.py
@@ -25,3 +25,23 @@ HEADER_200 = b'''HTTP/1.1 200 OK\n\n'''
 HEADER_404 = b'''HTTP/1.1 404 File Not Found\n\n'''
 
 
+
+class ProxyReadHandler (http.server.BaseHTTPRequestHandler):
+    __base = http.server.BaseHTTPRequestHandler
+    __base_handle = __base.handle
+
+
+class ThreadingHTTPServer (socketserver.ThreadingMixIn,
+                           http.server.HTTPServer):
+    pass
+
+
+def server_instance(bind_host, bind_port, Handler):
+    print(('Proxy started on %s:%s' % (bind_host, bind_port)))
+    server = ThreadingHTTPServer((bind_host, bind_port), Handler)
+    server.serve_forever()
+
+
+if __name__ == '__main__':
+    proxyread = Thread(target=server_instance, args=[bind_ip, port, ProxyReadHandler])
+    proxyread.start()
